1ce0bc3190a76aea5f6e39314eb31892ab9e02bc
Vítor Marçal <git@vitormarcal.com.br>
2018-11-29 10:33:31 -0200

    UPDATE name started class

--- src/main/java/br/com/vitormarcal/eltjob/App.java
+++ src/main/java/br/com/vitormarcal/eltjob/App.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EltjobApplication {
+public class App {
 
     public static void main(String[] args) {
-        SpringApplication.run(EltjobApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 }
