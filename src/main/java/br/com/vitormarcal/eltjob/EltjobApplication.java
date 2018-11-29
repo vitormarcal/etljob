bf371d0d3288d3cc6914c7483d57b73f367ace7a
vitormarcal <vitormarcal8@gmail.com>
2018-11-29 10:31:40 -0200

    first commit

---
+++ src/main/java/br/com/vitormarcal/eltjob/EltjobApplication.java
@@ -0,0 +1,12 @@
+package br.com.vitormarcal.eltjob;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EltjobApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EltjobApplication.class, args);
+    }
+}
---
+++ src/test/java/br/com/vitormarcal/eltjob/EltjobApplicationTests.java
@@ -0,0 +1,16 @@
+package br.com.vitormarcal.eltjob;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EltjobApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
